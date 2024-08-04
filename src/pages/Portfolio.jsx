58ff46dfc4ca9b5c55104c55443dab9811241eeb
design4reap <design4reap@gmail.com>
2024-08-04 17:48:42 +1000

    testing img path

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,28 +5,28 @@ import { Button } from 'react-bootstrap';
 const projects = [
   {
     title: 'Fetch Feeder Dog Tracker',
-    image: 'src/assets/images/fetchfeeder.png',
+    image: './src/assets/images/fetchfeeder.png',
     description: 'Fetch Feeder is an innovative dog tracker app designed to help dog owners effortlessly track their dogs diet and health.',
     deployedLink: 'https://fetch-feeder-1.onrender.com/',
     githubLink: 'https://github.com/Alex-Design-For-Reap/Fetch-Feeder-Dog-Tracker'
   },
   {
     title: 'Weather Dashboard',
-    image: 'src/assets/images/Current-weather-forecast.png',
+    image: '/src/assets/images/Current-weather-forecast.png',
     description: 'A weather dashboard that allows users to search for a city and see the current weather as well as a 5-day forecast.',
     deployedLink: 'https://weather-dashboard-56h1.onrender.com/',
     githubLink: 'https://github.com/Alex-Design-For-Reap/Weather-Dashboard'
   },
   {
     title: 'MCV Blog Post',
-    image: 'src/assets/images/Blog-post-portfolio.png',
+    image: '/src/assets/images/Blog-post-portfolio.png',
     description: 'A blog post app that allows users to create, edit, and delete blog posts.',
     deployedLink: 'https://mvc-blog-post.onrender.com/',
     githubLink: 'https://github.com/Alex-Design-For-Reap/MVC-Blog-Post'
   },
   {
   title: 'Note Taker Express.js',
-  image: 'src/assets/images/Note-taker-portfolio.png',
+  image: '/src/assets/images/Note-taker-portfolio.png',
   description: 'A Note taker app that allows users to write, save, and delete notes.',
   deployedLink: 'https://note-taker-express-js-j2ds.onrender.com/',
   githubLink: 'https://github.com/Alex-Design-For-Reap/note-taker-express-js'
